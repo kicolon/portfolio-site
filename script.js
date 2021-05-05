39cfd0e2a56d09b41320bc66acd35f0f27c7a648
kicolon <78152652+kicolon@users.noreply.github.com>
2021-05-05 12:27:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+(function(){
+    var words = [
+        ' artist',
+        ' graphic designer',
+        
+        ], i = 0;
+    setInterval(function(){
+        $('#changingword').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 3000);
+      
+})();
+
